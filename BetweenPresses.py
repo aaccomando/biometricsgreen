9d49d9af442f1387a05f7e2ce88ea295f545e6b2
Logan Lopez <lizardlab@yahoo.com>
2018-11-23 13:26:52 -0500

    Finished off code

--- BetweenPresses.py
+++ BetweenPresses.py
@@ -8,30 +8,57 @@ with open('output.csv', 'r') as f:
     reader = csv.reader(f)
     ev = list(reader)
 
-easyLag = 0
-easyCount = 0
-medLag = 0
-medCount = 0
-hardLag = 0
-hardCount = 0
+easyLag = {}
+easyCount = {}
+medLag = {}
+medCount = {}
+hardLag = {}
+hardCount = {}
 
 for x in range(1, len(ev) - 1):
     # only get avg lag bewtween presses of same passwd type
-    if ev[x-1][14] == ev[x][14]:
+    if ev[x-1][14] == ev[x][14] and ev[x-1][0] == ev[x][0]:
+        user = int(ev[x][0])
         if ev[x][14] == '0':
-            easyLag += int(ev[x][5]) - int(ev[x-1][4])
-            easyCount += 1
+            if user in easyLag:
+                easyLag[user] += int(ev[x][5]) - int(ev[x-1][4])
+                easyCount[user] += 1
+            else:
+                easyLag[user] = int(ev[x][5]) - int(ev[x-1][4])
+                easyCount[user] = 1
         elif ev[x][14] == '1':
-            medLag += int(ev[x][5]) - int(ev[x-1][4])
-            medCount += 1
+            if user in medLag:
+                medLag[user] += int(ev[x][5]) - int(ev[x-1][4])
+                medCount[user] += 1
+            else:
+                medLag[user] = int(ev[x][5]) - int(ev[x-1][4])
+                medCount[user] = 1
         elif ev[x][14] == '2':
-            hardLag += int(ev[x][5]) - int(ev[x-1][4])
-            hardCount += 1
+            if user in hardLag:
+                hardLag[user] += int(ev[x][5]) - int(ev[x-1][4])
+                hardCount[user] += 1
+            else:
+                hardLag[user] = int(ev[x][5]) - int(ev[x-1][4])
+                hardCount[user] = 1
+easyAvg = {}
+for key, value in easyLag.items():
+    easyAvg[key] = value / easyCount[key]
+medAvg = {}
+for key, value in medLag.items():
+    medAvg[key] = value / medCount[key]
+hardAvg = {}
+for key, value in hardLag.items():
+    hardAvg[key] = value / hardCount[key]
 
-easyAvg = easyLag/easyCount
-medAvg = medLag/medCount
-hardAvg = hardLag/hardCount
-
-print("Easy Average: {0}".format(easyAvg))
-print("Medium Average: {0}".format(medAvg))
-print("Hard Average: {0}".format(hardAvg))
+print('Easy Averages')
+print('===========================')
+for key, value in easyAvg.items():
+    print("User: {0}\t Average:{1}".format(key, value))
+print('Medium Averages')
+print('===========================')
+for key, value in medAvg.items():
+    print("User: {0}\t Average:{1}".format(key, value))
+print('Hard Averages')
+print('===========================')
+for key, value in medAvg.items():
+    print("User: {0}\t Average:{1}".format(key, value))
